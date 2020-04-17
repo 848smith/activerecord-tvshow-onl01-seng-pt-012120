ff26c691205d19e9874eefc6d5276b823a702090
Andrew Smith <848smith@gmail.com>
2020-04-17 04:39:00 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,19 +11,19 @@ class Show < ActiveRecord::Base
     Show.minumum("rating")
   end
   
-  def least_popular_show
+  def Show::least_popular_show
     Show.find(lowest_rating)
   end
   
-  def ratings_sum
+  def Show::ratings_sum
     Show.sum("ratings")
   end
   
-  def popular_shows
+  def Show::popular_shows
     Show.where("rating > 5")
   end
   
-  def shows_by_alphabetical_order
+  def Show::shows_by_alphabetical_order
     Show.order(name: :asc)
   end
 end
